6de4d04571e78956f214185f76ece404b012cf0c
Raffaella <raffaella.tran@dunnhumby.com>
2018-10-24 14:53:50 +0200

    populate each element in row view

--- app/src/main/java/com/example/rafaellat/kotlinexercise/AddressAdapter.kt
+++ app/src/main/java/com/example/rafaellat/kotlinexercise/AddressAdapter.kt
@@ -5,17 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import javax.sql.CommonDataSource
+import android.widget.TextView
+
+//import kotlinx.android.synthetic.list_item_address.*
 
 class AddressAdapter(private val context: Context,
-                     private val dataSource: ArrayList<String>): BaseAdapter() {
+                     private val dataSource: ArrayList<AddressModel>): BaseAdapter() {
 
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val rowView = inflater.inflate(R.layout.list_item_address, parent, false)
+        val addressModel= getItem(position) as AddressModel
+        val addressValue = rowView.findViewById(R.id.address_value) as TextView
+        val cityValue = rowView.findViewById(R.id.city_value) as TextView
 
+        addressValue.text = addressModel.address
+        cityValue.text = addressModel.city
         return rowView
     }
 
