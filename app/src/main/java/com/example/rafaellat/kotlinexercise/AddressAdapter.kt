80b14600a340b165ebcb119e486a581e10a2f039
Raffaella <raffaella.tran@dunnhumby.com>
2018-10-25 14:45:20 +0200

    add View Holder

--- app/src/main/java/com/example/rafaellat/kotlinexercise/AddressAdapter.kt
+++ app/src/main/java/com/example/rafaellat/kotlinexercise/AddressAdapter.kt
@@ -15,13 +15,17 @@ class AddressAdapter(
     private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val rowView = inflater.inflate(R.layout.list_item_address, parent, false)
-        val addressModel = getItem(position) as AddressModel
-        val addressTextView = rowView.findViewById(R.id.address_text_view) as TextView
-        val cityTextView = rowView.findViewById(R.id.city_text_view) as TextView
 
-        addressTextView.text = addressModel.address
-        cityTextView.text = addressModel.city
+        val addressModel = getItem(position) as AddressModel
+        val rowView = inflater.inflate(R.layout.list_item_address, parent, false)
+        val addressViewHolder = AddressViewHolder(
+            rowView.findViewById(R.id.address_text_view),
+            rowView.findViewById(R.id.city_text_view)
+        )
+        // inflate the layout
+        addressViewHolder.addressTextView.text = addressModel.address
+        addressViewHolder.cityTextView.text = addressModel.city
+        convertView?.tag = addressViewHolder
 
         return rowView
     }
@@ -37,5 +41,11 @@ class AddressAdapter(
     override fun getCount(): Int {
         return addressList.size
     }
+
+    data class AddressViewHolder @JvmOverloads constructor(
+        val addressTextView: TextView,
+        val cityTextView: TextView
+
+    )
 
 }
